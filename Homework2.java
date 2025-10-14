b16e74c2fccba2c33f773eb9534082f1a22806df
ShinHyeon-Jin <113018950+ShinHyeon-Jin@users.noreply.github.com>
2025-10-14 22:49:18 +0900

    Update Homework2.java
    10/14 코드 수정

--- Homework2.java
+++ Homework2.java
@@ -1,35 +1,32 @@
 import java.util.Scanner;
 
 class Student {
-    private String studentId;
-    private String name;
-    private String major;
-    private String phoneNumber;
+    int studentId;
+    String name;
+    String major;
+    int phoneNumber;
 
 
-    public String getStudentId() {
+    int getStudentId() {
         return studentId;
     }
 
-    public String getName() {
+    String getName() {
         return name;
     }
 
-    public String getMajor() {
+    String getMajor() {
         return major;
     }
 
-    public String getFormattedPhoneNumber() {
-        if (phoneNumber != null && phoneNumber.length() == 11) {
-            return phoneNumber.substring(0, 3) + "-" +
-                    phoneNumber.substring(3, 7) + "-" +
-                    phoneNumber.substring(7);
-        }
-        return phoneNumber;
+    String getFormattedPhoneNumber() {
+        String tempPhoneNumber = Integer.toString(phoneNumber);
+        return tempPhoneNumber = ("0"+tempPhoneNumber.substring(0,2)+"-"+
+                tempPhoneNumber.substring(2,6)+"-"+tempPhoneNumber.substring(6));
     }
 
 
-    public void setStudentId(String studentId) {
+    public void setStudentId(int studentId) {
         this.studentId = studentId;
     }
 
@@ -41,7 +38,7 @@ class Student {
         this.major = major;
     }
 
-    public void setPhoneNumber(String phoneNumber) {
+    public void setPhoneNumber(int phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 }
@@ -56,24 +53,31 @@ public class Homework2 {
             System.out.print("학생의 학번, 이름, 전공, 전화번호를 입력하세요: ");
             students[i] = new Student();
 
-            students[i].setStudentId(scanner.next());
+            students[i].setStudentId(Integer.parseInt(scanner.next()));
             students[i].setName(scanner.next());
             students[i].setMajor(scanner.next());
-            students[i].setPhoneNumber(scanner.next());
+            students[i].setPhoneNumber(Integer.parseInt(scanner.next()));
         }
 
         System.out.println("\n입력된 학생들의 정보는 다음과 같습니다.");
 
-        String[] ordinals = {"첫번째", "두번째", "세번째"};
 
-        for (int i = 0; i < students.length; i++) {
-            System.out.println(ordinals[i] + " 학생: " +
-                    students[i].getStudentId() + " " +
-                    students[i].getName() + " " +
-                    students[i].getMajor() + " " +
-                    students[i].getFormattedPhoneNumber());
-        }
+        System.out.println("첫번째 학생: " +
+                students[0].getStudentId() + " " +
+                students[0].getName() + " " +
+                students[0].getMajor() + " " +
+                students[0].getFormattedPhoneNumber());
 
-        scanner.close();
+        System.out.println("두번째 학생: " +
+                students[1].getStudentId() + " " +
+                students[1].getName() + " " +
+                students[1].getMajor() + " " +
+                students[1].getFormattedPhoneNumber());
+
+        System.out.println("세번째 학생: " +
+                students[2].getStudentId() + " " +
+                students[2].getName() + " " +
+                students[2].getMajor() + " " +
+                students[2].getFormattedPhoneNumber());
     }
 }
